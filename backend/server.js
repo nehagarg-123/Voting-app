cb706af65f32968c413aad9de774e2095fa4debd
nehagarg-123 <nehagarg6201@gmail.com>
2025-07-25 19:13:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}));
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
